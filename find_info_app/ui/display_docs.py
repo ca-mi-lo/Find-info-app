1027a880a9fef22c71d5d913d87665436b07ec35
Juan M Barrios <j.m.barrios@gmail.com>
2024-05-29 12:04:48 -0600

    feat(feedback): completed new feedback functionality by doc

--- find_info_app/ui/display_docs.py
+++ find_info_app/ui/display_docs.py
@@ -1,6 +1,6 @@
 import os
 import gettext
-from typing import Iterable
+from typing import Iterable, Optional
 
 from langchain_core.documents import Document
 import streamlit as st
@@ -19,17 +19,20 @@ else:
     feedback = feedback.BaseFeedback()
 
 
-def display_docs(docs: Iterable[Document]):
+def display_docs(docs: Iterable[Document], query: Optional[str] = None):
     for idx, doc in enumerate(docs):
+        # Add query to doc metadata
+        if query:
+            doc.metadata["query"] = query
         st.markdown(
-                _("**Page:** ")
-                + str(doc.metadata["page"] + 1)
-                + ";"
-                + 5 * "&nbsp;"
-                + _("**File:** _")
-                + doc.metadata["source"]
-                + "_"
-                )
+            _("**Page:** ")
+            + str(doc.metadata["page"] + 1)
+            + ";"
+            + 5 * "&nbsp;"
+            + _("**File:** _")
+            + doc.metadata["source"]
+            + "_"
+        )
         st.markdown(doc.page_content)
         b1, b2 = st.columns(2)
 
@@ -39,12 +42,11 @@ def display_docs(docs: Iterable[Document]):
             else:
                 st.toast(_(":exclamation: Failed to send feedback"))
         if b2.button(
-                ":thumbsdown:", use_container_width=True, key=f"feedback_down_{idx}"
-                ):
+            ":thumbsdown:", use_container_width=True, key=f"feedback_down_{idx}"
+        ):
             if feedback.send(-1, doc):
                 st.toast(_(":white_check_mark: Thanks!"))
             else:
                 st.toast(_(":exclamation: Failed to send feedback"))
 
         st.divider()
-
