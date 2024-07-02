d7030de5dcf46bcba207da5951537be1b358c269
ca-mi-lo <camilo.ab.sc@gmail.com>
2024-07-01 20:05:03 -0600

    MVP look&feel

--- find_info_app/graphs.py
+++ find_info_app/graphs.py
@@ -15,14 +15,11 @@ from bokeh.models import Legend, Label
 #pip install --force-reinstall --no-deps bokeh==2.4.3
 
 ############################################################
-class DataProcessor:
-    def __init__(self):
-        print("Hello World!")
-
-class Prepare_plot:
-    def __init__(self, path = "./papers/datasets/one_shot_classif.csv"):
+class Prepare_plot():
+    def __init__(self, path = "./papers/datasets/one_shot_classif.csv",  skip_metadata=True):
         print("Reading csv..")
         self.path = path
+        self.skip_metadata = skip_metadata
         try:
             self.df = pd.read_csv(path)
             print("Read successfully")
@@ -30,17 +27,23 @@ class Prepare_plot:
         except:
             print("Pending: Add debugger")
             print(path)
-            #self.df = p    d.read_csv(self.path)
-        
+    
         finally:
             self.pivot_df()
 
-    
+    def filter_metadata(self, skip_metadata=True):
+            """Filters data based on skip_metadata flag."""
+            if skip_metadata:
+                self.df = self.df[self.df.category != 'Metadatos']
+            return self.df
+
     def head_df(self,n=1):
         print(self.df.head(n))
     
     def pivot_df(self):
+        self.df2 = self.filter_metadata()
         self.df2 = self.df.copy().filter(['source', 'author','category','racional','page_content'])
+
         self.df2["file_name"]= self.df2.source.apply(lambda x: Path(x).name)
         self.df2 = self.df2.groupby(['file_name', 'category'])[['page_content']]\
         .count().rename({'page_content':'counts'}, axis=1)\
@@ -64,16 +67,23 @@ class Prepare_plot:
         return _self.df    
     
     def run_plot(self):
+        
         n_catego = len(self.column_names)
         original_palette = palettes.Category20[n_catego]
+
+        '''
         gray_color = "#F0F0F0" #"#E0E0E0"
         category_colors = {
             category: color
             for category, color in zip(self.column_names, original_palette)
             if category != "Metadatos"
         }
-        category_colors["Metadatos"] = gray_color
+        
+        if not self.skip_metadata:
+            category_colors["Metadatos"] = gray_color
+        
         custom_palette = tuple(category_colors[category] for category in OrderedDict(sorted(category_colors.items())))
+        '''
 
         source = ColumnDataSource(self.data)
 
@@ -87,7 +97,8 @@ class Prepare_plot:
             stackers=self.column_names, 
             x='index', 
             source=source,
-            color = custom_palette,
+            color = original_palette,
+            #color = custom_palette,
             muted_alpha=0.2,
             legend_label = [cat[:39] for cat in self.column_names]
             )
--- pages/summary_plot.py
+++ pages/summary_plot.py
@@ -6,11 +6,14 @@ from pathlib import Path
 import find_info_app.graphs as grf
 ###########################################################################
 
-st.set_page_config(page_title="Classifier", page_icon="📊", layout="wide")
+st.set_page_config(page_title="Classifier",
+                   page_icon="📊", 
+                   layout="wide"
+                   )
 
-#df_ = pd.read_csv("./papers/datasets/one_shot_classif.csv")
 path = "./papers/datasets/one_shot_classif.csv"
 st.subheader("Búsqueda por Categoría")
+
 
 # Init Session state
 ss = st.session_state
@@ -24,13 +27,30 @@ if "file_name" not in ss:
 if "df" not in ss:
     ss["df"] = pd.DataFrame()
 
+
+#Quizás debería de vivir en prompts.py
 def calculate_page_number(df, page_size=5):
   df["page_number"] = (df.reset_index(drop=True).index // page_size) + 1
   return df
 
-with st.form(key='my_form'):
-    my_plot = grf.Prepare_plot()
+
+def ui_show_debug():
+    st.checkbox("show debug section", key="show_debug")
+
+def prepare_pager():
+    
+    skip_metadata = st.sidebar.checkbox("Skip category 'Metadatos'", value=True) 
+    ss["skip_metadata"] = skip_metadata
+    my_plot = grf.Prepare_plot(skip_metadata=skip_metadata)
+    
+    if skip_metadata:
+        my_plot.filter_metadata(skip_metadata=ss["skip_metadata"])
+        #ss["df"] = ss["df"][ss["df"]["category"]!="Metadatos"]
+
     ss["df"] = my_plot.get_df()
+    
+    if skip_metadata:
+        ss["df"] = my_plot.filter_metadata(skip_metadata=ss["skip_metadata"])
     ss["df"]["file_name"]= ss["df"].source.apply(lambda x: Path(x).name)
     ss["df"] = ss["df"][['page_content','category','file_name', 'racional']]
 
@@ -45,25 +65,44 @@ with st.form(key='my_form'):
                         .dropna()
 
     choose_catego = st.sidebar.selectbox('Categoría:', catego) 
+    choose_file = st.sidebar.selectbox('File_name:', file_name)
+
     if choose_catego != 'all':
-        ss["df"] = ss["df"][ss["df"].category == choose_catego]
+        ss["df"] = ss["df"][ss["df"].category == choose_catego].reset_index(drop=True)
+        ss["df_filtered"] = ss["df"]
+        ss["df_filtered"] = ss["df_filtered"][ss["df"].file_name == choose_file]
         
 
-    choose_file = st.sidebar.selectbox('File_name:', file_name)
+    
     if choose_file != 'all':
-        ss["df"] = ss["df"][ss["df"].file_name == choose_file]
+        ss["df"] = ss["df"][ss["df"].file_name == choose_file].reset_index(drop=True)
+        ss["df_filtered"] = ss["df"]
+        ss["df_filtered"] = ss["df_filtered"][ss["df"].category == choose_catego]
 
 
-    ss["df"] = calculate_page_number(ss["df"])  
-    #WHY IS THIS DF paging with thwe 
-    # .copy() Apply calculation to a copy to avoid modifying original DataFrame
+    ss["df"] = calculate_page_number(ss["df"])
     page_size = 5
-    
-    current_page = st.number_input("Página:", min_value=1, max_value=int(len(ss["df"]) / page_size) + 1)
-    
-    df_filtered = ss["df"][ss["df"].page_number == current_page]
 
-    for index, row in df_filtered.iterrows():
+    current_page = st.number_input("Página:", min_value=1, max_value=int(len(ss["df"]) / page_size) + 1)
+
+    df_filtered = ss["df"][ss["df"].page_number == current_page]
+    ss["df_filtered"] = df_filtered
+
+#def render_pager():
+    header = []
+    for index, row in ss["df_filtered"].iterrows():
+        
+        file_name = ss["df_filtered"]["file_name"].unique()
+        
+        if (len(file_name)==1):
+            file_name =str(file_name[0])
+        else: row["file_name"]
+
+        ss["categos"] = ss["df_filtered"]["category"].unique()
+        if (len(ss["categos"])==1):
+            ss["categos"] ="**"+ str(ss["categos"][0])+ "**"
+        else: 
+            ss["categos"] = row["category"]
 
         text = row["page_content"]
         text = re.sub(r"\n\s*\n", "\n\n", text)  # Replace multiple newlines with a single one
@@ -71,13 +110,20 @@ with st.form(key='my_form'):
         text = text.replace("a ´",'á').replace("e ´",'é').replace("o ´",'ó').replace("i ´",'í').replace("ı ´","í")
         text = text.replace("A ´ ",'Á').replace("E ´",'É').replace("I ´",'Í').replace("o ´",'Ó')
         
-        header = f"**Chunk {index + 1}:**"
-        #with st.container(): 
-        st.markdown(header)
-        st.text(text)
-        st.divider()
-    
-    st.form_submit_button("pícale aquí")
+        
+        header = f"**Chunk {index + 1}:** _" + 5 * "&nbsp;" + file_name + "_" + 5*"&nbsp;" + str(ss["categos"])
+        st.markdown(header if isinstance(header, str) else header[-1])
+        #st.text(text)
+        font_size = '14px'
+        font_fam = 'Arial'
+        text_formated = f"""<span style="font-size: {font_size}; font-family: {font_fam};"> {text} </span>"""
+        st.markdown(text_formated, unsafe_allow_html=True)
 
-ss["df"] #= st.dataframe(ss["df"])
+        st.divider()
+
+prepare_pager()
+
+
+
+
 ############################################################################
